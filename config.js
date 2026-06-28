19800bcb44cce0f9802aeb5b16a3a77be8dde521
bharata65 <bharataa65@gmail.com>
2026-06-28 22:55:53 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCkA8HNQd8kDA8uLP6hNWgkjF78d3wKXbU",
-    authDomain: "bharat-digital-assets.firebaseapp.com",
-    projectId: "bharat-digital-assets",
-    // बाकी डिटेल्स...
-};
-
