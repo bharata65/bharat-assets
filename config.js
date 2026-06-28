609512ccc1ab5e21daf8b22fed50386bedb3b321
bharata65 <bharataa65@gmail.com>
2026-06-28 23:03:08 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCkA8HNQd8kDA8uLP6hNWgkjF78d3wKXbU",
+    authDomain: "bharat-digital-assets.firebaseapp.com",
+    projectId: "bharat-digital-assets",
+    storageBucket: "bharat-digital-assets.appspot.com",
+    messagingSenderId: "451769882363",
+    appId: "1:451769882363:web:f88db52e495d8456d5425e"
+};
+
