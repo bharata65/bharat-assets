cdd25d0b50164b673f51c40f90cd7a1c914237ff
bharata65 <bharataa65@gmail.com>
2026-06-28 20:04:34 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCkA8HNQd8kDA8uLP6hNWgkjF78d3wKXbU",
+    authDomain: "bharat-digital-assets.firebaseapp.com",
+    projectId: "bharat-digital-assets",
+    // बाकी डिटेल्स...
+};
+
