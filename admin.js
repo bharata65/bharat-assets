91eb5aa6afa9692bc1e4e50a2551b63abbe29e86
bharata65 <bharataa65@gmail.com>
2026-06-28 22:55:22 +0530

    Delete admin.js

--- admin.js
+++
@@ -1,9 +0,0 @@
-import { getFirestore, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-const db = getFirestore();
-
-window.updateMarket = async (shareId, newPrice) => {
-    const ref = doc(db, "shares", shareId);
-    await updateDoc(ref, { price: newPrice });
-    alert("Market Updated Real-time!");
-};
-
