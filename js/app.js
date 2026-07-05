b5b7abd16c35732c971f6e468f1e9ca8c9da67ff
bharata65 <bharataa65@gmail.com>
2026-07-05 08:53:24 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+async function loadPage(page){
+
+  const html = await fetch(
+    `./components/${page}.html`
+  ).then(r=>r.text());
+
+  document.getElementById("app")
+    .innerHTML = html;
+
+  if(page==="register" || page==="login"){
+    await import("./auth.js");
+  }
+}
+
+window.loadPage = loadPage;
+
+loadPage("register");
